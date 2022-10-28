37228e8cb44041026118632a6a80b71205a0f2ed
Camilla Fernandes <114497536+fdscamilla@users.noreply.github.com>
2022-10-27 21:43:37 -0300

    Update script-servicos.js

--- script/script-servicos.js
+++ script/script-servicos.js
@@ -1,4 +1,25 @@
 function enviarForm(){
-    document.getElementById(submit);
-    alert("Seu pedido foi enviado");
-}
+
+    var nome = document.getElementById("nome");
+    if (nome.value == "") {
+        return false
+    }
+
+    var email = document.getElementById("email");
+    if (email.value == "" || email.value.indexOf('@')==-1 || email.value.indexOf('.')==-1) {
+        return false
+    }
+
+    var fone = document.getElementById("fone");
+    if (fone.value < 11) {
+        return false
+    }
+
+
+    var modelo = document.getElementById("modelo");
+    if (selecionar-modelo.value == "") {
+        return false
+    }
+    return true
+
+}    
